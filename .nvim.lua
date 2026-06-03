ed3479b537a03837b728b7602fa572975f06fe67
voidarc <admin@voidarc.co.uk>
2026-06-03 20:32:40 +0100

    setup

---
+++ .nvim.lua
@@ -0,0 +1,3 @@
+require("lspconfig").qmlls.setup()
+
+require("nvim-treesitter").install({ "qmljs" })
--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
 
           buildInputs = with pkgs; [
             quickshell
+            kdePackages.qtdeclarative
           ];
 
           # Environment variables
